8ce65b71ddf032d7c2232c41cc0839dc89b08737
Wilfred Mutwiri <Wilfred Mutwiri>
2024-03-19 21:30:55 +0300

    Add loading state on login and signup

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -5,32 +5,39 @@ const Signup = () => {
     const [email,setEmail]=useState('')
     const [password,setPassword]=useState('')
     const {signup,isLoading,error}=useSignup()
+    const [load,setload]=useState(true)
 
     const handleSubmit=async(e)=>{
         e.preventDefault()
         await signup(email,password)
+        setload(false)
+
     }
     return (
-        <form class="bg-gray-200 mt-16 w-72 p-5 m-auto rounded-md shadow-sm shadow-black" onSubmit={handleSubmit}>
+        <div>
+        <p class={`${load ? 'hidden':'block' } text-center pt-10 text-green-600`}>Please wait...this may take some time..</p>
+        <form class="bg-gray-200 mt-14 w-72 p-5 m-auto rounded-md shadow-sm shadow-black" onSubmit={handleSubmit}>
         <h3 class="text-center text-orange-600 font-semibold">Sign Up</h3>
         <label>Email</label><br/>
         <input 
         type="email"
         onChange={(e)=>setEmail(e.target.value)} 
         value={email}
-        class="w-60 rounded-md shadow-sm shadow-blue-700"
+        class="w-60 p-1 rounded-md shadow-sm shadow-blue-700"
         /><br/>
         <label>Password</label><br/>
         <input 
         type="password"
         onChange={(e)=>setPassword(e.target.value)}
         value={password} 
-        class="w-60 rounded-md shadow-sm shadow-blue-700"
+        class="w-60 p-1 rounded-md shadow-sm shadow-blue-700"
         /><br/>
         <button disabled={isLoading} class="mt-5 bg-orange-500 p-1 rounded-md shadow-md shadow-black">Sign up</button>
 
         {error && <div class=" bg-red-300 mt-5 p-1 rounded-md text-red-700">{error}</div>}
+        <p class="pt-4 pb-4">Already have an account! <a class="text-red-500" href="/login">Login</a></p>
         </form>
+        </div>
     );
 }
  
